c8f620b57672e0ae4bdf74a10a67e125f9921da7
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-05-13 15:26:26 +0000

    bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "yogiben:pretty-email",
   summary: "Send pretty emails",
-  version: "0.0.4",
+  version: "0.0.5",
   git: "https://github.com/yogiben/meteor-pretty-email"
 });
 
